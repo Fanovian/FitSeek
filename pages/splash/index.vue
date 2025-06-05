2487ab2a5e6e4324a22cdc54c3da97c4ad35f1aa
YueShengjia <EclipseYue196@gmail.com>
2025-06-05 20:57:05 +0800

    Onlaunch page

---
+++ pages/splash/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="splash-container">
+    <image class="splash-logo" src="/static/logo.png" mode="widthFix" />
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'Splash',
+  mounted() {
+    setTimeout(() => {
+      // 跳转到登录页
+      uni.reLaunch({ url: '/pages/login/index' });
+    }, 3000);
+  }
+}
+</script>
+
+<style scoped>
+.splash-container {
+  width: 100vw;
+  height: 100vh;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.splash-logo {
+  width: 220rpx;
+  max-width: 60vw;
+  display: block;
+}
+</style>
